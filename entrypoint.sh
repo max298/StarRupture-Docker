13cae858691b7c8414eee26b03c90b207409599a
Max Kammler <max.kammler@me.com>
2025-12-08 18:57:10 +0100

    add simple entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# fixme: add to dockerfile, should not be part of this file
-printf "Ensure vcrun2022 is installed...\n"
-winetricks -q vcrun2022
-
-# fixme: add new app-id for release
+# fixme: add hard code app-id after release
 printf "Updating game %s...\n" "$app_id"
 steamcmd +login "$username" +app_update "$app_id" validate +quit +force_install_dir /server
+
+printf "Starting server...\n"
+xvfb-run wine "/server/Steam/steamapps/common/StarRupture Playtest Dedicated Server/StarRuptureServerEOS.exe"

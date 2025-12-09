c72031d52d8846088eb42f857bb8d875a7d351f7
Max Kammler <max.kammler@me.com>
2025-12-09 12:29:23 +0100

    add required argument for port, cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# fixme: add hard code app-id after release
-printf "Updating game %s...\n" "$app_id"
-steamcmd +login "$username" +app_update "$app_id" validate +quit +force_install_dir /server
+printf "Updating game %s...\n" "$STEAMAPPID"
+steamcmd +login "$username" +app_update "$STEAMAPPID" validate +quit +force_install_dir /server
 
 printf "Starting server...\n"
-xvfb-run wine "/server/Steam/steamapps/common/StarRupture Playtest Dedicated Server/StarRuptureServerEOS.exe"
+xvfb-run wine "/server/Steam/steamapps/common/StarRupture Playtest Dedicated Server/StarRuptureServerEOS.exe" -Log -port=7777

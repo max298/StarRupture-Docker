4b3e59e85c52d8b08c3aba5ff5edbfcce96a4fdd
Max Kammler <max.kammler@icloud.com>
2026-01-07 11:47:29 +0100

    use winecmd.exe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+# check existance of steamcmd.exe
+if [ ! -e "./steamcmd/steamcmd.exe" ]; then
+  # get steamcmd.exe (because linux-steamcmd won't download windows-app)
+  printf "Downloading steamcmd...\n"
+  curl --output "steamcmd.zip" https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip
+  unzip steamcmd.zip
+  # cleanup
+  rm steamcmd.zip
+  # move to persistant storage
+  mv steamcmd.exe /server/steamapps
+fi
+
 printf "Updating game %s...\n" "$STEAMAPPID"
-steamcmd +login "$username" +app_update "$STEAMAPPID" validate +quit +force_install_dir /server
+wine /server/steamapps/steamcmd.exe +force_install_dir /server +login anonymous +app_update "$STEAMAPPID" validate +quit
 
 printf "Starting server...\n"
-xvfb-run wine "/server/Steam/steamapps/common/StarRupture Playtest Dedicated Server/StarRuptureServerEOS.exe" -Log -port=7777
+xvfb-run wine "/server/steamapps/common/StarRupture Dedicated Server/StarRuptureServerEOS.exe" -Log -port=7777
